fd481d4fdce31cfaaf414d24ebbba0b2ff99a20c
seth <getchoo@tuta.io>
2023-03-17 21:13:55 -0400

    feat(flake): init nix flake

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "getchoo's personal website";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            alejandra.enable = true;
+            prettier.enable = true;
+            editorconfig-checker.enable = true;
+          };
+        };
+      };
+
+      devShells = let
+        inherit (pkgs) mkShell;
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
+      in {
+        default = mkShell {
+          inherit shellHook;
+          packages = with pkgs; [
+            alejandra
+            deno
+            nodePackages.prettier
+          ];
+        };
+      };
+    });
+}
