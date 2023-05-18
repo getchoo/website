5f5b83b62ceb88a44681dba6b3618fe464c162b8
seth <getchoo@tuta.io>
2023-05-18 08:54:37 -0400

    flake: add support for `git rev-parse` during build
    🤡

--- nix/default.nix
+++ nix/default.nix
@@ -1,17 +1,28 @@
-{mkYarnPackage, ...}:
-mkYarnPackage rec {
-  pname = "getchoo-website";
-
-  src = builtins.path {
-    path = ../.;
-    name = "getchoo-website-source";
-  };
-
-  packageJSON = src + "/package.json";
-  yarnLock = src + "/yarn.lock";
-
-  buildPhase = ''
-    export HOME="$(mktemp -d)"
-    yarn --offline build
+{
+  self,
+  mkYarnPackage,
+  writeShellScriptBin,
+  ...
+}: let
+  gitRev = writeShellScriptBin "git" ''
+    echo ${self.rev or "dirty"};
   '';
-}
+in
+  mkYarnPackage rec {
+    pname = "getchoo-website";
+
+    src = builtins.path {
+      path = ../.;
+      name = "getchoo-website-source";
+    };
+
+    packageJSON = src + "/package.json";
+    yarnLock = src + "/yarn.lock";
+
+    nativeBuildInputs = [gitRev];
+
+    buildPhase = ''
+      export HOME="$(mktemp -d)"
+      yarn --offline build
+    '';
+  }
