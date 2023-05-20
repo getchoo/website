4c9e7e5e97e0c5b100a8301649084bb1040c644d
seth <getchoo@tuta.io>
2023-05-20 00:01:59 -0400

    switch to pnpm

--- flake.nix
+++ flake.nix
@@ -25,10 +25,6 @@
 
     forAllSystems = nixpkgs.lib.genAttrs systems;
     nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
-
-    packageFn = pkgs: {
-      getchoo-website = pkgs.callPackage ./nix {inherit self;};
-    };
   in {
     checks = forAllSystems (system: {
       pre-commit-check = pre-commit-hooks.lib.${system}.run {
@@ -45,36 +41,20 @@
     devShells = forAllSystems (system: let
       pkgs = nixpkgsFor.${system};
       inherit (pkgs) mkShell;
-      cache = pkgs.fetchYarnDeps {
-        yarnLock = ./yarn.lock;
-        sha256 = "sha256-yhhuIfXjJhSfA8lweJ/0iD1qhnS3Th7P4zT+8iiWB/8=";
-      };
     in {
       default = mkShell {
         inherit (self.checks.${system}.pre-commit-check) shellHook;
         packages = with pkgs;
         with nodePackages; [
-          alejandra
-          cache
           eslint
           just
           nodejs
           prettier
-          yarn
+          pnpm
         ];
       };
     });
 
-    nixosModules.default = import ./nix/module.nix;
-
-    packages = forAllSystems (s: let
-      pkgs = nixpkgsFor.${s};
-      p = packageFn pkgs;
-    in {
-      inherit (p) getchoo-website;
-      default = p.getchoo-website;
-    });
-
-    overlays.default = _: packageFn;
+    formatter = forAllSystems (s: nixpkgs.${s}.alejandra);
   };
 }
