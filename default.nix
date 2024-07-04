a219a4eb3f849e599abf868660690a8926a4bc10
seth <getchoo@tuta.io>
2024-07-04 15:56:24 -0400

    nix: use channel path by default on stable nix

--- default.nix
+++ default.nix
@@ -1,16 +1,11 @@
-let
-  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  fetchTree = import ./nix/fetchTree.nix;
-  flakeSources = builtins.mapAttrs (_: node: fetchTree node.locked) lock.nodes;
-in
 {
-  pkgs ? import sources.nixpkgs {
+  pkgs ? import nixpkgs {
     inherit system;
     config = { };
     overlays = [ ];
   },
+  nixpkgs ? <nixpkgs>,
   system ? builtins.currentSystem,
-  sources ? flakeSources,
 }:
 {
   website = pkgs.callPackage ./nix/package.nix { };
--- shell.nix
+++ shell.nix
@@ -1,16 +1,11 @@
-let
-  lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  fetchTree = import ./nix/fetchTree.nix;
-  flakeSources = builtins.mapAttrs (_: node: fetchTree node.locked) lock.nodes;
-in
 {
-  pkgs ? import sources.nixpkgs {
+  pkgs ? import nixpkgs {
     inherit system;
     config = { };
     overlays = [ ];
   },
+  nixpkgs ? <nixpkgs>,
   system ? builtins.currentSystem,
-  sources ? flakeSources,
   formatter ? pkgs.nixfmt-rfc-style,
 }:
 pkgs.mkShellNoCC {
