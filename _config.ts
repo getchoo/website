975a9d8d914760ba7c8789c12d982503ee300e66
seth <getchoo@tuta.io>
2024-07-19 03:27:28 -0400

    add robots.txt

--- _config.ts
+++ _config.ts
@@ -2,6 +2,7 @@ import lume from "lume/mod.ts";
 import inline from "lume/plugins/inline.ts";
 import metas from "lume/plugins/metas.ts";
 import minify_html from "lume/plugins/minify_html.ts";
+import robots from "lume/plugins/robots.ts";
 import sitemap from "lume/plugins/sitemap.ts";
 
 const site = lume({ src: "./src" });
@@ -9,6 +10,7 @@ const site = lume({ src: "./src" });
 site.use(inline())
 	.use(metas())
 	.use(minify_html())
+	.use(robots())
 	.use(sitemap());
 
 site.copy("static", ".");
