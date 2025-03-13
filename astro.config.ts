898f1b95c7200a321eb686738e06283fc224959a
Seth Flynn <getchoo@tuta.io>
2025-03-12 20:08:01 -0400

    feat: use tailwind and daisyui

--- astro.config.ts
+++ astro.config.ts
@@ -2,8 +2,13 @@ import { defineConfig } from "astro/config";
 
 import sitemap from "@astrojs/sitemap";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
 	site: process.env.CF_PAGES_URL || "https://getchoo.com",
 	integrations: [sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
